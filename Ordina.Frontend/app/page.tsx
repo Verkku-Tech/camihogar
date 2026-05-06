6d6d5af0157e8da072e27402e0538fff1f513830
cheo jose <cheo1894@gmail.com>
2026-05-06 15:32:57 -0400

    se creo scrapper de monedas en BCV

--- Ordina.Frontend/app/page.tsx
+++ Ordina.Frontend/app/page.tsx
@@ -1,10 +1,11 @@
-import { ProtectedRoute } from "@/components/auth/protected-route"
-import { Dashboard } from "@/components/dashboard/dashboard"
+import { ProtectedRoute } from "@/components/auth/protected-route";
+import { Dashboard } from "@/components/dashboard/dashboard";
+import { useEffect } from "react";
 
 export default function HomePage() {
   return (
     <ProtectedRoute>
       <Dashboard />
     </ProtectedRoute>
-  )
+  );
 }
